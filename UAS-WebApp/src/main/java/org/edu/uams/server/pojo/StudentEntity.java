646623e91f59b9ec7cc20c563e1ded572d8394d8
javadeveloper080 <javadeveloper080@gmail.com>
2015-04-08 23:04:24 +0530

    added searchPage.jsp

--- UAS-WebApp/src/main/java/org/edu/uams/server/pojo/StudentEntity.java
+++ UAS-WebApp/src/main/java/org/edu/uams/server/pojo/StudentEntity.java
@@ -4,252 +4,160 @@ package org.edu.uams.server.pojo;
 import java.io.Serializable;
 
 import java.util.Collection;
+import java.util.Date;
 import javax.persistence.Basic;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
+import org.edu.uams.server.api.FacultyType;
+import org.edu.uams.server.api.GenderType;
 
 /**
  *
  * @author Mohammed.Tauseef
  */
-@Entity
+@Entity(name = "Student")
 @Table(name = "student")
-@XmlRootElement
-@NamedQueries({
-    @NamedQuery(name = "StudentEntity.findAll", query = "SELECT s FROM StudentEntity s"),
-    @NamedQuery(name = "StudentEntity.findByAdmnNum", query = "SELECT s FROM StudentEntity s WHERE s.admnNum = :admnNum"),
-    @NamedQuery(name = "StudentEntity.findByRollNum", query = "SELECT s FROM StudentEntity s WHERE s.rollNum = :rollNum"),
-    @NamedQuery(name = "StudentEntity.findByStudentName", query = "SELECT s FROM StudentEntity s WHERE s.studentName = :studentName"),
-    @NamedQuery(name = "StudentEntity.findByBranchName", query = "SELECT s FROM StudentEntity s WHERE s.branchName = :branchName"),
-    @NamedQuery(name = "StudentEntity.findBySem", query = "SELECT s FROM StudentEntity s WHERE s.sem = :sem"),
-    @NamedQuery(name = "StudentEntity.findByPhoto", query = "SELECT s FROM StudentEntity s WHERE s.photo = :photo"),
-    @NamedQuery(name = "StudentEntity.findByPath", query = "SELECT s FROM StudentEntity s WHERE s.path = :path")})
 public class StudentEntity implements Serializable {
     
     public static final String FIND_ALL="StudentEntity.findAll";
     
     private static final long serialVersionUID = 1L;
     
-    @Column(name = "admn_num")
-    private Long admnNum;
     
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = false)
+    private long id;
+    
     @Basic(optional = false)
-    @Column(name = "roll_num")
+    @Column(name = "admn_num",unique = true,nullable = false)
+    private Long admnNum;
+    
+    
+    @Basic(optional = false)
+    @Column(name = "roll_num",unique = true,nullable = false)
     private String rollNum;
     
-    @Column(name = "student_name")
-    private String studentName;
+    @Column(name = "first_name", nullable = false, length = 100)
+    private String firstName;
+    
+    @Column(name = "last_name", nullable = false, length = 100)
+    private String lastName;
+    
+    @Column(name = "last_name", nullable = false, length = 100)
+    private String fatherName;
+    
+    
+    @Column(name = "last_name", nullable = false, length = 100)
+    private String motherName;
     
     @Basic(optional = false)
-    @Column(name = "branch_name")
-    private String branchName;
-    
-    @Column(name = "sem")
-    private Short sem;
+    @Column(name = "mobile",nullable = false)
+    private long mobile;
     
     @Basic(optional = false)
-    @Column(name = "photo")
-    private long photo;
+    @Column(name = "dob")
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date dob;
     
-    @Column(name = "path")
-    private String path;
     
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "rollNum")
-    private Collection<StudentOtherDetailsEntity> studentOtherDetailsTabCollection;
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "rollNum")
-    private Collection<StudentFeeEntity> studentFeeTabCollection;
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "rollNum")
-    private Collection<StudentPersonalDetailsEntity> studentPersonalDetailsTabCollection;
-    @OneToOne(cascade = CascadeType.ALL, mappedBy = "studentTab")
-    private StudentDegreequalificationDetailsEntity studentDegreequalificationDetailsTab;
-    @OneToOne(cascade = CascadeType.ALL, mappedBy = "studentTab")
-    private StudentCorrAddressDetailsEntity studentCorrAddressDetailsTab;
-    @OneToOne(cascade = CascadeType.ALL, mappedBy = "studentTab")
-    private StudentDiplomaqualificationDetailsEntity studentDiplomaqualificationDetailsTab;
-    @OneToOne(cascade = CascadeType.ALL, mappedBy = "studentTab")
-    private StudentPermAddressDetailsEntity studentPermAddressDetailsTab;
-    @OneToOne(cascade = CascadeType.ALL, mappedBy = "studentTab")
-    private StudentSscqualificationDetailsEntity studentSscqualificationDetailsTab;
-    @OneToOne(cascade = CascadeType.ALL, mappedBy = "studentTab")
-    private StudentInterqualificationDetailsEntity studentInterqualificationDetailsTab;
-
-    public StudentEntity() {
+    @Basic(optional = false)
+    @Column(name = "gender_type", length = 100, nullable = false)
+    @Enumerated(EnumType.STRING)
+    private GenderType genderType;
+    
+    public long getId() {
+        return id;
     }
-
-    public StudentEntity(String rollNum) {
-        this.rollNum = rollNum;
+    
+    public void setId(long id) {
+        this.id = id;
     }
-
-    public StudentEntity(String rollNum, String branchName, long photo) {
-        this.rollNum = rollNum;
-        this.branchName = branchName;
-        this.photo = photo;
-    }
-
+    
     public Long getAdmnNum() {
         return admnNum;
     }
-
+    
     public void setAdmnNum(Long admnNum) {
         this.admnNum = admnNum;
     }
-
+    
     public String getRollNum() {
         return rollNum;
     }
-
+    
     public void setRollNum(String rollNum) {
         this.rollNum = rollNum;
     }
-
-    public String getStudentName() {
-        return studentName;
-    }
-
-    public void setStudentName(String studentName) {
-        this.studentName = studentName;
-    }
-
-    public String getBranchName() {
-        return branchName;
-    }
-
-    public void setBranchName(String branchName) {
-        this.branchName = branchName;
-    }
-
-    public Short getSem() {
-        return sem;
-    }
-
-    public void setSem(Short sem) {
-        this.sem = sem;
-    }
-
-    public long getPhoto() {
-        return photo;
-    }
-
-    public void setPhoto(long photo) {
-        this.photo = photo;
-    }
-
-    public String getPath() {
-        return path;
-    }
-
-    public void setPath(String path) {
-        this.path = path;
-    }
-
-    @XmlTransient
-    public Collection<StudentOtherDetailsEntity> getStudentOtherDetailsTabCollection() {
-        return studentOtherDetailsTabCollection;
-    }
-
-    public void setStudentOtherDetailsTabCollection(Collection<StudentOtherDetailsEntity> studentOtherDetailsTabCollection) {
-        this.studentOtherDetailsTabCollection = studentOtherDetailsTabCollection;
-    }
-
-    @XmlTransient
-    public Collection<StudentFeeEntity> getStudentFeeTabCollection() {
-        return studentFeeTabCollection;
-    }
-
-    public void setStudentFeeTabCollection(Collection<StudentFeeEntity> studentFeeTabCollection) {
-        this.studentFeeTabCollection = studentFeeTabCollection;
-    }
-
-    @XmlTransient
-    public Collection<StudentPersonalDetailsEntity> getStudentPersonalDetailsTabCollection() {
-        return studentPersonalDetailsTabCollection;
-    }
-
-    public void setStudentPersonalDetailsTabCollection(Collection<StudentPersonalDetailsEntity> studentPersonalDetailsTabCollection) {
-        this.studentPersonalDetailsTabCollection = studentPersonalDetailsTabCollection;
-    }
-
-    public StudentDegreequalificationDetailsEntity getStudentDegreequalificationDetailsTab() {
-        return studentDegreequalificationDetailsTab;
-    }
-
-    public void setStudentDegreequalificationDetailsTab(StudentDegreequalificationDetailsEntity studentDegreequalificationDetailsTab) {
-        this.studentDegreequalificationDetailsTab = studentDegreequalificationDetailsTab;
-    }
-
-    public StudentCorrAddressDetailsEntity getStudentCorrAddressDetailsTab() {
-        return studentCorrAddressDetailsTab;
-    }
-
-    public void setStudentCorrAddressDetailsTab(StudentCorrAddressDetailsEntity studentCorrAddressDetailsTab) {
-        this.studentCorrAddressDetailsTab = studentCorrAddressDetailsTab;
-    }
-
-    public StudentDiplomaqualificationDetailsEntity getStudentDiplomaqualificationDetailsTab() {
-        return studentDiplomaqualificationDetailsTab;
-    }
-
-    public void setStudentDiplomaqualificationDetailsTab(StudentDiplomaqualificationDetailsEntity studentDiplomaqualificationDetailsTab) {
-        this.studentDiplomaqualificationDetailsTab = studentDiplomaqualificationDetailsTab;
-    }
-
-    public StudentPermAddressDetailsEntity getStudentPermAddressDetailsTab() {
-        return studentPermAddressDetailsTab;
-    }
-
-    public void setStudentPermAddressDetailsTab(StudentPermAddressDetailsEntity studentPermAddressDetailsTab) {
-        this.studentPermAddressDetailsTab = studentPermAddressDetailsTab;
-    }
-
-    public StudentSscqualificationDetailsEntity getStudentSscqualificationDetailsTab() {
-        return studentSscqualificationDetailsTab;
-    }
-
-    public void setStudentSscqualificationDetailsTab(StudentSscqualificationDetailsEntity studentSscqualificationDetailsTab) {
-        this.studentSscqualificationDetailsTab = studentSscqualificationDetailsTab;
-    }
-
-    public StudentInterqualificationDetailsEntity getStudentInterqualificationDetailsTab() {
-        return studentInterqualificationDetailsTab;
-    }
-
-    public void setStudentInterqualificationDetailsTab(StudentInterqualificationDetailsEntity studentInterqualificationDetailsTab) {
-        this.studentInterqualificationDetailsTab = studentInterqualificationDetailsTab;
+    
+    public String getFirstName() {
+        return firstName;
     }
     
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (rollNum != null ? rollNum.hashCode() : 0);
-        return hash;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof StudentEntity)) {
-            return false;
-        }
-        StudentEntity other = (StudentEntity) object;
-        if ((this.rollNum == null && other.rollNum != null) || (this.rollNum != null && !this.rollNum.equals(other.rollNum))) {
-            return false;
-        }
-        return true;
+    
+    public String getLastName() {
+        return lastName;
     }
-
-    @Override
-    public String toString() {
-        return "org.edu.uams.pojo.StudentEntity[ rollNum=" + rollNum + " ]";
+    
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+    public String getFatherName() {
+        return fatherName;
+    }
+    
+    public void setFatherName(String fatherName) {
+        this.fatherName = fatherName;
+    }
+    
+    public String getMotherName() {
+        return motherName;
+    }
+    
+    public void setMotherName(String motherName) {
+        this.motherName = motherName;
+    }
+    
+    public long getMobile() {
+        return mobile;
+    }
+    
+    public void setMobile(long mobile) {
+        this.mobile = mobile;
+    }
+    
+    public Date getDob() {
+        return dob;
+    }
+    
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+    
+    public GenderType getGenderType() {
+        return genderType;
+    }
+    
+    public void setGenderType(GenderType genderType) {
+        this.genderType = genderType;
     }
     
 }
