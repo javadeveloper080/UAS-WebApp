694d24f545751f2a2f625e381a64c2d158672ded
javadeveloper080 <javadeveloper080@gmail.com>
2015-04-10 00:05:08 +0530

    StudentAddressEntity added

--- UAS-WebApp/src/main/java/org/edu/uams/server/pojo/StudentEntity.java
+++ UAS-WebApp/src/main/java/org/edu/uams/server/pojo/StudentEntity.java
@@ -15,6 +15,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import org.edu.uams.server.api.GenderType;
+import org.edu.uams.server.api.SeatCategoryType;
 
 /**
  *
@@ -59,14 +60,42 @@ public class StudentEntity implements Serializable {
     @Column(name = "mother_name", nullable = false, length = 100)
     private String motherName;
     
-    @Basic(optional = false)
-    @Column(name = "mobile",nullable = false)
-    private long mobile;
+    @Basic(optional = true)
+    @Column(name = "student_mob",nullable = true)
+    private long studentMob;
     
     @Basic(optional = false)
     @Column(name = "dob")
     @Temporal(javax.persistence.TemporalType.DATE)
     private Date dob;
+    
+    @Basic(optional = false)
+    @Column(name = "nationality" ,nullable = false)
+    private String nationality;
+    
+    @Basic(optional = true)
+    @Column(name = "father_occup")
+    private String fatherOccup;
+    
+    @Basic(optional = true)
+    @Column(name = "mother_occup")
+    private String motherOccup;
+    
+    
+    @Basic(optional = true)
+    @Column(name = "parent_mob")
+    private String parentMob;
+    
+    
+    @Basic(optional = false)
+    @Column(name = "email")
+    private String email;
+    
+    
+    @Basic(optional = false)
+    @Column(name = "seat_category_type", length = 100, nullable = false)
+    @Enumerated(EnumType.STRING)
+    private SeatCategoryType seatCategoryType;
     
     
     @Basic(optional = false)
@@ -130,13 +159,6 @@ public class StudentEntity implements Serializable {
         this.motherName = motherName;
     }
     
-    public long getMobile() {
-        return mobile;
-    }
-    
-    public void setMobile(long mobile) {
-        this.mobile = mobile;
-    }
     
     public Date getDob() {
         return dob;
@@ -153,5 +175,62 @@ public class StudentEntity implements Serializable {
     public void setGenderType(GenderType genderType) {
         this.genderType = genderType;
     }
+
+    public long getStudentMob() {
+        return studentMob;
+    }
+
+    public void setStudentMob(long studentMob) {
+        this.studentMob = studentMob;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getFatherOccup() {
+        return fatherOccup;
+    }
+
+    public void setFatherOccup(String fatherOccup) {
+        this.fatherOccup = fatherOccup;
+    }
+
+    public String getMotherOccup() {
+        return motherOccup;
+    }
+
+    public void setMotherOccup(String motherOccup) {
+        this.motherOccup = motherOccup;
+    }
+
+    public String getParentMob() {
+        return parentMob;
+    }
+
+    public void setParentMob(String parentMob) {
+        this.parentMob = parentMob;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public SeatCategoryType getSeatCategoryType() {
+        return seatCategoryType;
+    }
+
+    public void setSeatCategoryType(SeatCategoryType seatCategoryType) {
+        this.seatCategoryType = seatCategoryType;
+    }
+    
     
 }
